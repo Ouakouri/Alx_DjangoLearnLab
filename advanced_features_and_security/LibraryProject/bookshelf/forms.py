1346573a0dec3110baaf24a1eeca464c35e13d5c
abdelkarim Ouakouri <abdelkarim5114@gmail.com>
2024-08-26 00:15:26 +0100

    add seciruty add forms2

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,4 +1,5 @@
 from django import forms
 
-class MyForm(forms.Form):
-    query = forms.CharField(max_length=100)
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
