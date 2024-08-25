06412340042e6f954753d5981361ab2e10b13592
abdelkarim Ouakouri <abdelkarim5114@gmail.com>
2024-08-25 23:37:10 +0100

    add seciruty7

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -3,13 +3,13 @@ from django.contrib.auth.decorators import permission_required
 from django.shortcuts import render
 
 @permission_required('app_name.can_edit', raise_exception=True)
-def edit_my_model(request, pk):
+def edit_book_list(request, pk):
  
-    return render(request, 'edit_my_model.html', context)
+    return render(request, 'edit_book_list.html', context)
 
 @permission_required('app_name.can_create', raise_exception=True)
-def create_my_model(request):
+def create_books(request):
 
-    return render(request, 'create_my_model.html', context)
+    return render(request, 'create_books.html', context)
 
 # Create your views here.
