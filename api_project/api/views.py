b8bc42a324d2e6fcc590bde89784587abf943174
abdelkarim Ouakouri <abdelkarim5114@gmail.com>
2024-09-08 23:19:48 +0100

    api project3

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -8,3 +8,4 @@ from .serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()  
     serializer_class = BookSerializer 
+    permission_classes = [IsAuthenticated] 
