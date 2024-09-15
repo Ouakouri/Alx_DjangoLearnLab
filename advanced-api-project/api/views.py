26ca1a47e8de94b1c52ec618d88294a085c6d1ae
abdelkarim Ouakouri <abdelkarim5114@gmail.com>
2024-09-15 16:24:48 +0100

    api advenced project filters1

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -8,6 +8,7 @@ from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework import status
 from rest_framework.response import Response
+from django_filters import rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.filters import OrderingFilter
