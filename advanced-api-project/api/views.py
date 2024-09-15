290c2411177a27d00f5c80ca84ddc1621a411636
abdelkarim Ouakouri <abdelkarim5114@gmail.com>
2024-09-15 13:01:37 +0100

    api advenced project modification 1

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import status
+from rest_framework.response import Response
+
+class BookListView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        if serializer.validated_data['publication_year'] > 2024:
+            raise serializers.ValidationError("Publication year can't be in the future.")
+        serializer.save()
+        
+class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_update(self, serializer):
+        if serializer.validated_data['publication_year'] > 2024:
+            raise serializers.ValidationError("Publication year can't be in the future.")
+        serializer.save()
