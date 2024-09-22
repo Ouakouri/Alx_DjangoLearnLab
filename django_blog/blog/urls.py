a7f466a4f1459dc10927ebe11843206baf097be2
abdelkarim Ouakouri <abdelkarim5114@gmail.com>
2024-09-22 11:59:45 +0100

    2django blog5

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,8 +14,8 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # عرض تفاصيل المنشور
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),  # تحديث المنشور
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),  # حذف المنشور
-    path('post/<int:post_id>/comments/new/', add_comment, name='add-comment'),
+    path('post/<int:pk>/comments/new/', add_comment, name='add-comment'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='update-comment'),
     path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='edit-comment'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete-comment'),
 ]
-
