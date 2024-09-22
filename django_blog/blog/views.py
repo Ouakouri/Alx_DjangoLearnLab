c07b98e0f962f78b971d8f89a1055e3bbcce9174
abdelkarim Ouakouri <abdelkarim5114@gmail.com>
2024-09-22 10:34:05 +0100

    2django blog1

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -40,4 +40,54 @@ def profile(request):
         form = UserProfileForm(instance=request.user)
     return render(request, 'blog/profile.html', {'form': form})
 
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from .models import Post
+
+# ListView for displaying all blog posts
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-published_date']  # Show latest posts first
+
+# DetailView for a single blog post
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+# CreateView for new blog post creation
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
+
+    # Set the author of the post to the logged-in user
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+# UpdateView for editing a post by the author
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
+
+    # Ensure the author is editing their own post
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+# DeleteView for deleting a post by the author
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('posts')  # Redirect to the post list after deletion
+
+    # Ensure only the author can delete their post
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
 
